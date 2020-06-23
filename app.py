689b823ba7c3f36575b411ab8601cae608cf3bd8
Bealby <mgbealby@gmail.com>
2020-06-23 10:42:49 +0000

    Setup connection to Mongo data. Created catch_log.html

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 import os
 if os.path.exists("env.py"):
     import env
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 
 app = Flask(__name__)
+
 app.config["MONGO_DBNAME"] = 'magnet_fishing'
 app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return 'Hello World'
+@app.route('/get_catches')
+def get_catches():
+    return render_template("catch_log.html", catch_log=mongo.db.catch_log.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
