2d72d657016de35aa18d5c01cf94a37d96820d35
Bealby <mgbealby@gmail.com>
2020-06-25 19:33:47 +0000

    Fixed and set-up emailjs in sendEmail.js

--- static/js/sendEmail.js
+++ static/js/sendEmail.js
@@ -1,11 +1,10 @@
 function sendMail(contactForm) {
-     emailjs.init("user_UW97WmP3GsBepuyB8Vffd");
-     emailjs.send("gmail", "magnet_fishing", {
+    emailjs.init("user_ltp9VN3EuB6AFgRZdXukW");
+    emailjs.send("gmail", "magnet_fishing", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "comment": contactForm.comment.value
     })
-
     .then(
         function(response) {
             console.log("SUCCESS", response);
