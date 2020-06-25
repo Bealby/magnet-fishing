4f0bf5dbe93afa3ba9a0ac45341f49c910df4e94
Bealby <mgbealby@gmail.com>
2020-06-25 16:53:45 +0000

    Create javascript for emailjs

---
+++ static/js/sendEmail.js
@@ -0,0 +1,18 @@
+function sendMail(contactForm) {
+     emailjs.init("user_UW97WmP3GsBepuyB8Vffd");
+     emailjs.send("gmail", "magnet_fishing", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "comment": contactForm.comment.value
+    })
+
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
