92a3cf45cfe349751118449d004e42e08ccdec9b
Bealby <mgbealby@gmail.com>
2020-07-01 15:45:29 +0000

    Fixed background image diwplqy

--- static/js/sendEmail.js
+++ static/js/sendEmail.js
@@ -9,6 +9,7 @@ function sendMail(contactForm) {
         function(response) {
             document.getElementById("myForm").reset();
             console.log("SUCCESS", response);
+        
         },
         function(error) {
             console.log("FAILED", error);
