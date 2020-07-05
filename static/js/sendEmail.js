e08ed2f03cefd4cd903be2697ef118b6107a36fe
Bealby <mgbealby@gmail.com>
2020-07-05 19:29:43 +0000

    Added comments to sendEmail.js

--- static/js/sendEmail.js
+++ static/js/sendEmail.js
@@ -1,3 +1,6 @@
+// Script for EmailJS API as well as alerts
+// Environ variable set up for EMAILJS_KEY
+
 function sendMail(contactForm) {
     emailjs.send("gmail", "magnet_fishing", {
         "from_name": contactForm.name.value,
@@ -6,13 +9,13 @@ function sendMail(contactForm) {
     })
     .then(
         function(response) {
-            document.getElementById("myForm").reset();
-            swal("Message Sent", "We will be in touch soon!", "success");
+            document.getElementById("myForm").reset();  // Clears contact form after message sent
+            swal("Message Sent", "We will be in touch soon!", "success"); // Message Alert for 'Message Sent'
             console.log("SUCCESS", response);
         
         },
         function(error) {
-            swal("Message Fail", "Please check connection and try again", "error");
+            swal("Message Fail", "Please check connection and try again", "error"); // Message Alert for 'Message Fail'
             console.log("FAILED", error);
         }
     );
